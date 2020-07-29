eba6b226dc95cc8f7c89b7dfab192763ad952fc8
Aman Shekhar <Aman Shekhar>
2020-07-29 19:01:47 +0530

    using useState

---
+++ src/components/SongList.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import NewSongForm from './NewSongForm';
+
+const SongList = () => {
+    const [songs, setSongs] = useState([
+        { title: 'almost home', id: 1},
+        { title: 'memory gospel', id: 2},
+        { title: 'this wild darkness', id: 3}
+    ]);
+
+    const addSong = (title) => {
+        setSongs([...songs, { title: title, id: 4}]);
+    }
+    return ( 
+        <div className='song-list'>
+            <ul>
+                {songs.map(song => {
+                    return (<li key={song.id}>{song.title}</li>);
+                })}
+            </ul>
+
+            <NewSongForm addSong={addSong}/>
+        </div>
+     );
+}
+ 
+export default SongList;
