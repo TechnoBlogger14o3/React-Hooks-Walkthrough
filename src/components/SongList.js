4336dda412082090c6850a8892dd45266d6a073c
Aman Shekhar <Aman Shekhar>
2020-07-30 12:47:51 +0530

    using a useEffect just for a demo purpose

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,19 +2,28 @@ import React, { useState, useEffect } from 'react';
 import NewSongForm from './NewSongForm';
 
 const SongList = () => {
+    
     var randomID = Math.floor(Math.random() * 100) + 1;
+
     const [songs, setSongs] = useState([
         { title: 'almost home', id: 1},
         { title: 'memory gospel', id: 2},
         { title: 'this wild darkness', id: 3}
     ]);
+    
+    const[age, setAge] = useState(20);
+
     const addSong = (title) => {
         setSongs([...songs, { title: title, id: randomID}]);
     }
+    
+    useEffect(() => {
+        console.log('useEffect Songs', songs);
+    }, [songs])
 
     useEffect(() => {
-        console.log('useEffect ran', songs);
-    })
+        console.log('useEffect Age', age);
+    }, [age])
 
     return ( 
         <div className='song-list'>
@@ -25,6 +34,8 @@ const SongList = () => {
             </ul>
 
             <NewSongForm addSong={addSong}/>
+
+            <button onClick={() => setAge(age + 1)}>Add 1 to Age = {age}</button>
         </div>
      );
 }
